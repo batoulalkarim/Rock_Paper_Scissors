0a41340364b1dbbc63ad2e028d2e0abf730b3163
Batoul Alkarim <batoulalkarim@Batouls-MBP.lan>
2021-12-07 20:00:27 -0500

    Assigns number from 0-2 to rock paper scissors

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -6,3 +6,14 @@ const getUserChoice = userInput => {
         console.log('Error! Please enter rock, paper, or scissors.');
     }
 };
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+        case 0:
+            return 'rock';
+        case 1: 
+            return 'paper';
+        case 2: 
+            return 'scissors';        
+    }
+};
